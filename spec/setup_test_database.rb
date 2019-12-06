9a3b6e174377d4726137029c54f1551a57e1bcdf
ajbacon <47368135+ajbacon@users.noreply.github.com>
2019-12-06 09:01:52 +0000

    fix test bug, all tests now pass"

--- spec/setup_test_database.rb
+++ spec/setup_test_database.rb
@@ -4,6 +4,8 @@ require 'pg'
 def setup_test_database
   p 'Setting up test database...'
 
+  DatabaseConnection.setup(dbname: 'bookmark_manager_test')
+
   #clear the bookmarks table
   DatabaseConnection.query("TRUNCATE bookmarks;")
 end
